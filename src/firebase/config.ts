e3514c15509a4fe6148f80546359cba9e445c0b0
Kompleksanda Akinyomola <Bynarikompleks@gmail.com>
2025-11-09 20:32:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2005470510-9d9a5",
+  "appId": "1:838484508906:web:d13c4aec3feb421e00d793",
+  "apiKey": "AIzaSyCllZygyllkHr4kv54GUuADLvDXkiZnKB4",
+  "authDomain": "studio-2005470510-9d9a5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "838484508906"
+};
