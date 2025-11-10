400ccd219b7ad9568d082e51f6c039df3e78d8c1
Kompleksanda Akinyomola <Bynarikompleks@gmail.com>
2025-11-10 09:12:13 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,5 @@
 import { redirect } from 'next/navigation';
-import { getApps, initializeApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { firebaseConfig } from '@/firebase/config';
 
-export default async function Home() {
-  if (typeof window !== 'undefined') {
-    if (!getApps().length) {
-      initializeApp(firebaseConfig);
-    }
-    const auth = getAuth();
-    
-    return new Promise<never>((resolve, reject) => {
-      const unsubscribe = onAuthStateChanged(auth, user => {
-        unsubscribe();
-        if (user) {
-          redirect('/dashboard');
-        } else {
-          redirect('/login');
-        }
-      }, reject);
-    });
-  }
-
+export default function Home() {
   redirect('/login');
 }
