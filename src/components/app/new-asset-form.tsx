8e3054534e8e679f223d0ad0eaa7b9ac91a8f446
Kompleksanda Akinyomola <Bynarikompleks@gmail.com>
2025-11-10 10:06:40 +0000

    Try fixing this error: `Runtime Error: Error: useMemo is not defined. Er

--- src/components/app/new-asset-form.tsx
+++ src/components/app/new-asset-form.tsx
@@ -3,6 +3,7 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { useMemo } from 'react';
 import { Button } from '@/components/ui/button';
 import {
   Form,
